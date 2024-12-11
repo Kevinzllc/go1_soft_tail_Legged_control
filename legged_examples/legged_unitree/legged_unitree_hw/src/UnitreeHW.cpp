2b724aaa8e83863496d732510ec979eece74f3ec
Kevinzllc <159160853+Kevinzllc@users.noreply.github.com>
2024-12-11 15:22:21 +0800

    Update UnitreeHW.cpp

--- legged_examples/legged_unitree/legged_unitree_hw/src/UnitreeHW.cpp
+++ legged_examples/legged_unitree/legged_unitree_hw/src/UnitreeHW.cpp
@@ -1,206 +1,156 @@
-
 //
 // Created by qiayuan on 1/24/22.
 //
 
-// 原有 SDK 头文件
-// #include "legged_unitree_hw/UnitreeHW.h"
-
-// 新的 SDK 头文件
-#include "fdsc_utils/free_dog_sdk_h.hpp"  // 修改为新的 SDK 头文件
+#include "legged_unitree_hw/UnitreeHW.h"
+#include "free_dog_sdk/unitreeConnect.hpp"
+#include "free_dog_sdk/lowState.hpp"
+#include "free_dog_sdk/lowCmd.hpp"
+#include "free_dog_sdk/highState.hpp"
+#include "free_dog_sdk/highCmd.hpp"
 
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Int16MultiArray.h>
 
 namespace legged {
+
 bool UnitreeHW::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_hw_nh) {
-  if (!LeggedHW::init(root_nh, robot_hw_nh)) {
-    return false;
-  }
+    if (!LeggedHW::init(root_nh, robot_hw_nh)) {
+        return false;
+    }
 
-  robot_hw_nh.getParam("power_limit", powerLimit_);
+    robot_hw_nh.getParam("power_limit", powerLimit_);
 
-  setupJoints();
-  setupImu();
-  setupContactSensor(robot_hw_nh);
+    setupJoints();
+    setupImu();
+    setupContactSensor(robot_hw_nh);
 
-  // 初始化新的 UDP 通信
-  udp_ = std::make_shared<FREE_DOG_SDK::UDP>();  // 新的 SDK UDP 初始化方法
+    connection_ = std::make_shared<FDSC::UnitreeConnection>("LOW_WIRED_DEFAULTS");
+    connection_->startRecv();
 
-  // 使用新的命令结构
-  udp_->InitCmdData(lowCmd_);
+    std::string robot_type;
+    root_nh.getParam("robot_type", robot_type);
 
-  std::string robot_type;
-  root_nh.getParam("robot_type", robot_type);
+    if (robot_type == "go1") {  
+        safety_ = std::make_shared<FDSC::Safety>(FDSC::DogType::YourType);
+    } else {
+        ROS_FATAL("Unknown robot type: %s", robot_type.c_str());
+        return false;
+    }
 
-  // 根据 robot_type 初始化不同的安全控制
-  if (robot_type == "go1") {
-    safety_ = std::make_shared<FREE_DOG_SDK::Safety>(FREE_DOG_SDK::LeggedType::Go1);  // 修改为新的 SDK 接口
-  } else {
-    ROS_FATAL("Unknown robot type: %s", robot_type.c_str());
-    return false;
-  }
-
-  joyPublisher_ = root_nh.advertise<sensor_msgs::Joy>("/joy", 10);
-  contactPublisher_ = root_nh.advertise<std_msgs::Int16MultiArray>(std::string("/contact"), 10);
-  return true;
+    joyPublisher_ = root_nh.advertise<sensor_msgs::Joy>("/joy", 10);
+    contactPublisher_ = root_nh.advertise<std_msgs::Int16MultiArray>("/contact", 10);
+    return true;
 }
 
 void UnitreeHW::read(const ros::Time& time, const ros::Duration& /*period*/) {
-  udp_->Recv();  // 接收数据
-  udp_->GetRecv(lowState_);  // 获取接收到的状态信息
+    auto data = connection_->getData();
 
-  // 更新关节信息
-  for (int i = 0; i < 12; ++i) {
-    jointData_[i].pos_ = lowState_.motorState[i].q;
-    jointData_[i].vel_ = lowState_.motorState[i].dq;
-    jointData_[i].tau_ = lowState_.motorState[i].tauEst;
-  }
+    for (int i = 0; i < 12; ++i) {
+        jointData_[i].pos_ = data[i].position;
+        jointData_[i].vel_ = data[i].velocity;
+        jointData_[i].tau_ = data[i].torque;
+    }
 
-  // 更新 IMU 数据
-  imuData_.ori_[0] = lowState_.imu.quaternion[1];
-  imuData_.ori_[1] = lowState_.imu.quaternion[2];
-  imuData_.ori_[2] = lowState_.imu.quaternion[3];
-  imuData_.ori_[3] = lowState_.imu.quaternion[0];
-  imuData_.angularVel_[0] = lowState_.imu.gyroscope[0];
-  imuData_.angularVel_[1] = lowState_.imu.gyroscope[1];
-  imuData_.angularVel_[2] = lowState_.imu.gyroscope[2];
-  imuData_.linearAcc_[0] = lowState_.imu.accelerometer[0];
-  imuData_.linearAcc_[1] = lowState_.imu.accelerometer[1];
-  imuData_.linearAcc_[2] = lowState_.imu.accelerometer[2];
+    imuData_.ori_ = { data.imu.orientation[0], data.imu.orientation[1], data.imu.orientation[2], data.imu.orientation[3] };
+    imuData_.angularVel_ = { data.imu.angularVelocity[0], data.imu.angularVelocity[1], data.imu.angularVelocity[2] };
+    imuData_.linearAcc_ = { data.imu.linearAcceleration[0], data.imu.linearAcceleration[1], data.imu.linearAcceleration[2] };
 
-  // 更新接触传感器状态
-  for (size_t i = 0; i < CONTACT_SENSOR_NAMES.size(); ++i) {
-    contactState_[i] = lowState_.footForce[i] > contactThreshold_;
-  }
+    for (size_t i = 0; i < CONTACT_SENSOR_NAMES.size(); ++i) {
+        contactState_[i] = data.contactStates[i] > contactThreshold_;
+    }
 
-  // 清零控制命令以避免不安全的操作
-  std::vector<std::string> names = hybridJointInterface_.getNames();
-  for (const auto& name : names) {
-    HybridJointHandle handle = hybridJointInterface_.getHandle(name);
-    handle.setFeedforward(0.);
-    handle.setVelocityDesired(0.);
-    handle.setKd(3.);
-  }
-
-  updateJoystick(time);
-  updateContact(time);
+    updateJoystick(time);
+    updateContact(time);
 }
 
 void UnitreeHW::write(const ros::Time& /*time*/, const ros::Duration& /*period*/) {
-  // 更新关节命令
-  for (int i = 0; i < 12; ++i) {
-    lowCmd_.motorCmd[i].q = static_cast<float>(jointData_[i].posDes_);
-    lowCmd_.motorCmd[i].dq = static_cast<float>(jointData_[i].velDes_);
-    lowCmd_.motorCmd[i].Kp = static_cast<float>(jointData_[i].kp_);
-    lowCmd_.motorCmd[i].Kd = static_cast<float>(jointData_[i].kd_);
-    lowCmd_.motorCmd[i].tau = static_cast<float>(jointData_[i].ff_);
-  }
+    std::vector<uint8_t> cmdData;
 
-  // 安全保护 (注释掉原来的安全控制部分)
-  // safety_->PositionLimit(lowCmd_);
-  // safety_->PowerProtect(lowCmd_, lowState_, powerLimit_);
+    for (int i = 0; i < 12; ++i) {
+        lowCmd_.motorCmd[i].q = static_cast<float>(jointData_[i].posDes_);
+        lowCmd_.motorCmd[i].dq = static_cast<float>(jointData_[i].velDes_);
+        lowCmd_.motorCmd[i].Kp = static_cast<float>(jointData_[i].kp_);
+        lowCmd_.motorCmd[i].Kd = static_cast<float>(jointData_[i].kd_);
+        lowCmd_.motorCmd[i].tau = static_cast<float>(jointData_[i].ff_);
+    }
 
-  udp_->SetSend(lowCmd_);  // 发送控制命令
-  udp_->Send();  // 发送数据包
+    safety_->PositionLimit(lowCmd_);
+    safety_->PowerProtect(lowCmd_, lowState_, powerLimit_);
+    connection_->send(lowCmd_.buildCmd());
 }
 
 bool UnitreeHW::setupJoints() {
-  for (const auto& joint : urdfModel_->joints_) {
-    int leg_index = 0;
-    int joint_index = 0;
-    if (joint.first.find("RF") != std::string::npos) {
-      leg_index = FREE_DOG_SDK::FR_;  // 修改为新的 SDK 接口
-    } else if (joint.first.find("LF") != std::string::npos) {
-      leg_index = FREE_DOG_SDK::FL_;  // 修改为新的 SDK 接口
-    } else if (joint.first.find("RH") != std::string::npos) {
-      leg_index = FREE_DOG_SDK::RR_;  // 修改为新的 SDK 接口
-    } else if (joint.first.find("LH") != std::string::npos) {
-      leg_index = FREE_DOG_SDK::RL_;  // 修改为新的 SDK 接口
-    } else {
-      continue;
-    }
+    for (const auto& joint : urdfModel_->joints_) {
+        int leg_index = getLegIndex(joint.first);
+        int joint_index = getJointIndex(joint.first);
+        if (leg_index < 0 || joint_index < 0) continue;
 
-    if (joint.first.find("HAA") != std::string::npos) {
-      joint_index = 0;
-    } else if (joint.first.find("HFE") != std::string::npos) {
-      joint_index = 1;
-    } else if (joint.first.find("KFE") != std::string::npos) {
-      joint_index = 2;
-    } else {
-      continue;
+        int index = leg_index * 3 + joint_index;
+        hardware_interface::JointStateHandle state_handle(joint.first, &jointData_[index].pos_, &jointData_[index].vel_, &jointData_[index].tau_);
+        jointStateInterface_.registerHandle(state_handle);
+        hybridJointInterface_.registerHandle(HybridJointHandle(state_handle, &jointData_[index].posDes_, &jointData_[index].velDes_,
+                                                               &jointData_[index].kp_, &jointData_[index].kd_, &jointData_[index].ff_));
     }
-
-    int index = leg_index * 3 + joint_index;
-    hardware_interface::JointStateHandle state_handle(joint.first, &jointData_[index].pos_, &jointData_[index].vel_,
-                                                      &jointData_[index].tau_);
-    jointStateInterface_.registerHandle(state_handle);
-    hybridJointInterface_.registerHandle(HybridJointHandle(state_handle, &jointData_[index].posDes_, &jointData_[index].velDes_,
-                                                           &jointData_[index].kp_, &jointData_[index].kd_, &jointData_[index].ff_));
-  }
-  return true;
+    return true;
 }
 
 bool UnitreeHW::setupImu() {
-  imuSensorInterface_.registerHandle(hardware_interface::ImuSensorHandle("base_imu", "base_imu", imuData_.ori_, imuData_.oriCov_,
-                                                                         imuData_.angularVel_, imuData_.angularVelCov_, imuData_.linearAcc_,
-                                                                         imuData_.linearAccCov_));
-  imuData_.oriCov_[0] = 0.0012;
-  imuData_.oriCov_[4] = 0.0012;
-  imuData_.oriCov_[8] = 0.0012;
-
-  imuData_.angularVelCov_[0] = 0.0004;
-  imuData_.angularVelCov_[4] = 0.0004;
-  imuData_.angularVelCov_[8] = 0.0004;
-
-  return true;
+    imuSensorInterface_.registerHandle(hardware_interface::ImuSensorHandle("base_imu", "base_imu", imuData_.ori_, imuData_.oriCov_,
+                                                                           imuData_.angularVel_, imuData_.angularVelCov_, imuData_.linearAcc_,
+                                                                           imuData_.linearAccCov_));
+    imuData_.oriCov_ = {0.0012, 0, 0, 0, 0.0012, 0, 0, 0, 0.0012};
+    imuData_.angularVelCov_ = {0.0004, 0, 0, 0, 0.0004, 0, 0, 0, 0.0004};
+    imuData_.linearAccCov_ = {0.0004, 0, 0, 0, 0.0004, 0, 0, 0, 0.0004};
+    return true;
 }
 
 bool UnitreeHW::setupContactSensor(ros::NodeHandle& nh) {
-  nh.getParam("contact_threshold", contactThreshold_);
-  for (size_t i = 0; i < CONTACT_SENSOR_NAMES.size(); ++i) {
-    contactSensorInterface_.registerHandle(ContactSensorHandle(CONTACT_SENSOR_NAMES[i], &contactState_[i]));
-  }
-  return true;
+    nh.getParam("contact_threshold", contactThreshold_);
+    for (size_t i = 0; i < CONTACT_SENSOR_NAMES.size(); ++i) {
+        contactSensorInterface_.registerHandle(ContactSensorHandle(CONTACT_SENSOR_NAMES[i], &contactState_[i]));
+    }
+    return true;
 }
 
 void UnitreeHW::updateJoystick(const ros::Time& time) {
-  if ((time - lastJoyPub_).toSec() < 1 / 50.) {
-    return;
-  }
-  lastJoyPub_ = time;
-
-  xRockerBtnDataStruct keyData;
-  memcpy(&keyData, &lowState_.wirelessRemote[0], 40);
-  sensor_msgs::Joy joyMsg;  // Pack as same as Logitech F710
-  joyMsg.axes.push_back(-keyData.lx);
-  joyMsg.axes.push_back(keyData.ly);
-  joyMsg.axes.push_back(-keyData.rx);
-  joyMsg.axes.push_back(keyData.ry);
-  joyMsg.buttons.push_back(keyData.btn.components.X);
-  joyMsg.buttons.push_back(keyData.btn.components.A);
-  joyMsg.buttons.push_back(keyData.btn.components.B);
-  joyMsg.buttons.push_back(keyData.btn.components.Y);
-  joyMsg.buttons.push_back(keyData.btn.components.L1);
-  joyMsg.buttons.push_back(keyData.btn.components.R1);
-  joyMsg.buttons.push_back(keyData.btn.components.L2);
-  joyMsg.buttons.push_back(keyData.btn.components.R2);
-  joyMsg.buttons.push_back(keyData.btn.components.select);
-  joyMsg.buttons.push_back(keyData.btn.components.start);
-  joyPublisher_.publish(joyMsg);
+    if ((time - lastJoyPub_).toSec() < 1 / 50.) {
+        return;
+    }
+    lastJoyPub_ = time;
+    xRockerBtnDataStruct keyData;
+    memcpy(&keyData, &lowState_.wirelessRemote[0], 40);
+    sensor_msgs::Joy joyMsg;  
+    joyMsg.axes.push_back(-keyData.lx);
+    joyMsg.axes.push_back(keyData.ly);
+    joyMsg.axes.push_back(-keyData.rx);
+    joyMsg.axes.push_back(keyData.ry);
+    joyMsg.buttons.push_back(keyData.btn.components.X);
+    joyMsg.buttons.push_back(keyData.btn.components.A);
+    joyMsg.buttons.push_back (keyData.btn.components.B);
+    joyMsg.buttons.push_back (keyData.btn.components.Y);
+    joyMsg.buttons.push_back (keyData.btn.components.L1);
+    joyMsg.buttons.push_back (keyData.btn.components.R1);
+    joyMsg.buttons.push_back (keyData.btn.components.L2);
+    joyMsg.buttons.push_back (keyData.btn.components.R2);
+    joyMsg.buttons.push_back (keyData.btn.components.select);
+    joyMsg.buttons.push_back (keyData.btn.components.start);
+    joyPublisher_.publish(joyMsg);
 }
 
 void UnitreeHW::updateContact(const ros::Time& time) {
-  if ((time - lastContactPub_).toSec() < 1 / 50.) {
-    return;
-  }
-  lastContactPub_ = time;
+    if ((time - lastContactPub_).toSec() < 1 / 50.) {
+        return;
+    }
+    lastContactPub_ = time;
 
-  std_msgs::Int16MultiArray contactMsg;
-  for (size_t i = 0; i < CONTACT_SENSOR_NAMES.size(); ++i) {
-    contactMsg.data.push_back(lowState_.footForce[i]);
-  }
-  contactPublisher_.publish(contactMsg);
+    std_msgs::Int16MultiArray contactMsg;
+    for (size_t i = 0; i < CONTACT_SENSOR_NAMES.size(); ++i) {
+        contactMsg.data.push_back(lowState_.footForce[i]);
+    }
+    contactPublisher_.publish(contactMsg);
 }
 
 }  // namespace legged
+
+
